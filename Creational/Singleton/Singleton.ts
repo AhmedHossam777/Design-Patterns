0f79a5cfcc088f68f52110df3c14467e2b4289de
ahmed hossam <ahmedhossam9226@gmail.com>
2024-10-07 14:48:57 +0300

    creational/Singleton

--- Creational/Singleton/Singleton.ts
+++ Creational/Singleton/Singleton.ts
@@ -1,34 +1,30 @@
 class Singleton {
-	private static instance: Singleton;
+	static instance: Singleton;
 
 	private constructor() {}
 
-	private static _value: number;
-
-	get value(): number {
-		return Singleton._value;
-	}
-
-	set value(value: number) {
-		Singleton._value = value;
-	}
-
-	public static getInstance(): Singleton {
+	public static get getInstance(): Singleton {
 		if (!Singleton.instance) {
 			Singleton.instance = new Singleton();
 		}
+
 		return Singleton.instance;
+	}
+
+	public someBusinessLogic() {
+		// ...
 	}
 }
 
-let instance1 = Singleton.getInstance();
-let instance2 = Singleton.getInstance();
+function clientCode() {
+	const s1 = Singleton.instance;
+	const s2 = Singleton.instance;
 
-instance1.value = 10;
+	if (s1 === s2) {
+		console.log('Singleton works, both variables contain the same instance.');
+	} else {
+		console.log('Singleton failed, variables contain different instances.');
+	}
+}
 
-console.log(instance1.value); // 10
-console.log(instance2.value); // 10
-
-console.log(instance1 === instance2); // true
-
-// the both instance must have the same value if they are not that means that the singleton is not implemented well
+clientCode();
